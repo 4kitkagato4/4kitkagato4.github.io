c5ababe56c0c41b347770e5e670bc59b3132d63d
J Albert <57366785+4kitkagato4@users.noreply.github.com>
2020-01-22 00:19:05 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-$homepage = file_get_contents('https://tourbuilder.withgoogle.com/tour/ahJzfmd3ZWItdG91cmJ1a
-WxkZXJyEQsSBFRvdXIYgIDg4KrbqQsM');
+$homepage = file_get_contents('https://tourbuilder.withgoogle.com/tour/ahJzfmd3ZWItdG91cmJ1aWxkZXJyEQsSBFRvdXIYgIDg4KrbqQsM');
 echo $homepage;
 ?>
